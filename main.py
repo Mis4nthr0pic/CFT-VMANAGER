c27c4f164b724728a29a2a76231dad115c744581
Mi4nthr0pic <mis4nthr0proton.me>
2024-06-06 20:21:53 -0300

    fixing main provider requirements

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     ]
 
     processes = []
-    for listener_class, web3_url, contract_address, is_http in listeners:
+    for listener_class, web3_url, contract_address in listeners:
         provider = ConnectionProvider(listener_class.__name__, web3_url)
         process = multiprocessing.Process(target=run_listener, args=(listener_class, provider, contract_address))
         process.start()
